fd4371adba0ed263b1cce3188fcc9cf4db7b33e3
amitsachdeva45 <amitsachdeva45@gmail.com>
2019-09-07 21:37:45 -0400

    First commit

---
+++ code/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/amitsachdeva45/CollegeWork/Django/AllDjangoProjects/UniPortal/code/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
